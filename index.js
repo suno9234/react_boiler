84fa9c027db1d395b6bea2190aecdc4e459ae0d4
Suno <ssh9234@gmail.com>
2021-08-22 14:49:56 +0900

    move info

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express');
 const { mongo } = require('mongoose');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ssh9234:tnsghsmscjswo1@cluster0.zdzgv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const {User} = require('./models/user');
+
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI,
     {useNewUrlParser : true}).then(()=>console.log("db connected"))
                             .catch(err=>console.log(err));
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body);
+    user.save((err,userData)=>{
+        if(err) return res.json({succress : false, err});
+    })
+    return res.status(200).json({success : true});
+});
+
 app.get('/',(req,res)=>{
-    res.send('Hellow word');
+    res.json({"hello~ ": "Hi"});
 })
 
 app.listen(4000,(req,res)=>{
